c7728c466ff289fc8306ae6d4c66b904e6f673f3
bwigginswtsf@gmail.com <bwigginswtsf@gmail.com>
2024-04-19 00:16:05 -0400

    some pca stuff

--- examples/pcaaugmentation.py
+++ examples/pcaaugmentation.py
@@ -2,6 +2,9 @@
 import numpy as np
 from sklearn import decomposition
 
+#Shift the original data onto axes defined by the principal components
+#Augment the 3rd and 4th components by adding random value from a 0, 0.1 normal distribution
+#Transform the data back to the original axes and return
 def generateData(pca, x, start):
     original = pca.components_.copy()
     ncomp = pca.components_.shape[0]
@@ -16,21 +19,28 @@ def main():
     x = np.load("../data/iris/iris_features.npy")
     y = np.load("../data/iris/iris_labels.npy")
 
-    N = 120
+    #Separate into training and test data
+    N = 120 
     x_train = x[:N]
     y_train = y[:N]
     x_test = x[N:]
     y_test = y[N:]
 
+    #Initialize PCA and find component vectors for the data set
     pca = decomposition.PCA(n_components=4)
     pca.fit(x)
     print(pca.explained_variance_ratio_)
+
+    #2 most significant components are untouched and create 9 new sets
     start = 2
     nsets = 10
+    
+    #Create empty arrays to store the new sets
     nsamp = x_train.shape[0]
     newx = np.zeros((nsets*nsamp, x_train.shape[1]))
     newy = np.zeros(nsets*nsamp, dtype="uint8")
 
+    #Populate the first set with the original set and the subsequent 9 with augmented data
     for i in range(nsets):
         if (i == 0):
             newx[0:nsamp,:] = x_train
@@ -40,6 +50,7 @@ def main():
             newx[(i*nsamp):(i*nsamp+nsamp),:] = generateData(pca, x_train, start)
             newy[(i*nsamp):(i*nsamp+nsamp)] = y_train
     
+    #Randomize the ordering of all data
     idx = np.argsort(np.random.random(nsets*nsamp))
     newx = newx[idx]
     newy = newy[idx]
